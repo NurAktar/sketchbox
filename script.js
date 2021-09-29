2049f624f410ba947232b8d1f825da18c0405b2b
nuraktar <sknuraktar1999@gmail.com>
2021-09-29 22:15:46 +0530

    board out pen drawing fixed

--- script.js
+++ script.js
@@ -12,31 +12,59 @@ let draw_widht = "2";
 let is_drawing = false;
 let restore_array=[];
 let index=-1;
+let mhold = false;
 
 function change_color(element){
   draw_color= element.style.background;
 }
 
+//mouse hold state..
+document.addEventListener("mouseup",ms);
+document.addEventListener("mousedown",ms);
+function ms(event) {
+  if (event.type == "mousedown") {
+    mhold = true;
+  }
+  else {
+    mhold = false;
+  }
+}
+
+//touch state..
 canvas.addEventListener("touchstart",start,false);
 canvas.addEventListener("touchmove",draw,false);
 canvas.addEventListener("touchend",stop,false);
-
+//mouse events...
 canvas.addEventListener("mousedown",start,false);
 canvas.addEventListener("mousemove",draw,false);
-
 canvas.addEventListener("mouseup",stop,false);
 canvas.addEventListener("mouseout",stop,false);
+canvas.addEventListener("mouseenter",check_start,false);
+
+function check_start() {
+  if ( mhold == true ){
+    start();
+  }
+}
+// starting debug
 
 function start(event) {
   is_drawing = true;
   context.beginPath();
-  context.moveTo(event.clientX - canvas.offsetLeft,event.clientY - canvas.offsetTop);
+  context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
   event.preventDefault();
+
+  context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+  context.strokeStyle = draw_color;
+  context.lineWidth = draw_widht;
+  context.lineCap = "round";
+  context.lineJoin = "round";
+  context.stroke();
 }
 
 function draw(event){
   if(is_drawing){
-    context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
     context.strokeStyle = draw_color;
     context.lineWidth = draw_widht;
     context.lineCap = "round";
