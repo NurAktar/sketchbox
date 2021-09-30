d91be82adde2e4be1f7cacbe533c4a79b8ec1258
nuraktar <sknuraktar1999@gmail.com>
2021-09-30 14:27:24 +0530

    smooth pen

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const canvas = document.getElementById('canvas');
 canvas.width=window.innerWidth;
 canvas.height=700;
-
+// variables..
 let context = canvas.getContext("2d");
 let start_background_color = "white";
-context.fillStyle = start_background_color;
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 let draw_color = "black";
 let draw_widht = "2";
 let is_drawing = false;
 let restore_array=[];
 let index=-1;
 let mhold = false;
-
+// canvas fill..
+context.fillStyle = start_background_color;
+context.fillRect(0, 0, canvas.width, canvas.height);
+//element color change oninput..
 function change_color(element){
   draw_color= element.style.background;
 }
-
 //mouse hold state..
 document.addEventListener("mouseup",ms);
 document.addEventListener("mousedown",ms);
@@ -29,7 +28,6 @@ function ms(event) {
     mhold = false;
   }
 }
-
 //touch state..
 canvas.addEventListener("touchstart",start,false);
 canvas.addEventListener("touchmove",draw,false);
@@ -46,37 +44,33 @@ function check_start() {
     start();
   }
 }
-// starting debug
-
 function start(event) {
   is_drawing = true;
   context.beginPath();
   context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
-  event.preventDefault();
-
   context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
   context.strokeStyle = draw_color;
   context.lineWidth = draw_widht;
   context.lineCap = "round";
   context.lineJoin = "round";
   context.stroke();
+  event.preventDefault();
 }
-
 function draw(event){
   if(is_drawing){
-    context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
     context.strokeStyle = draw_color;
     context.lineWidth = draw_widht;
     context.lineCap = "round";
     context.lineJoin = "round";
+    context.shadowColor = draw_color;
+    context.shadowBlur = 2 ;
+    context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
     context.stroke();
   }
   event.preventDefault();
 }
-
 function stop(event){
   if (is_drawing){
-    context.stroke();
     context.closePath();
     is_drawing= false;
   }
@@ -86,7 +80,6 @@ function stop(event){
     index+=1;
   }
 }
-
 function clear_canvas() {
   index+=1;
   restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
