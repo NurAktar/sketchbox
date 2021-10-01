b0a524b4fce2533e715a08d23a2d62deb94be9a7
nuraktar <sknuraktar1999@gmail.com>
2021-10-01 16:19:51 +0530

    touch mobile devices fixed

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let is_drawing = false;
 let restore_array=[];
 let index=-1;
 let mhold = false;
+let pointX;
+let pointY;
 // canvas fill..
 context.fillStyle = start_background_color;
 context.fillRect(0, 0, canvas.width, canvas.height);
@@ -28,10 +30,75 @@ function ms(event) {
     mhold = false;
   }
 }
-//touch state..
-canvas.addEventListener("touchstart",start,false);
-canvas.addEventListener("touchmove",draw,false);
-canvas.addEventListener("touchend",stop,false);
+//touch functions..
+canvas.addEventListener("touchstart",t_start,false);
+canvas.addEventListener("touchmove",t_draw,false);
+canvas.addEventListener("touchend",t_stop,false);
+
+function t_start(event) {
+  is_drawing = true;
+  context.beginPath();
+  // context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
+  context.moveTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
+  context.lineTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
+  // context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+  pointX = event.touches[0].pageX;
+  // console.log(pointX);
+  pointY = event.touches[0].pageY;
+  // console.log(pointY);
+  context.strokeStyle = draw_color;
+  context.lineWidth = draw_width;
+  context.lineCap = "round";
+  context.lineJoin = "round";
+  context.shadowColor = draw_color;
+  context.shadowBlur = 2 ;
+  context.stroke();
+  event.preventDefault();
+}
+function t_draw(event){
+  // console.log( event.touches[0].pageX );
+  if(is_drawing){
+    // mmoved=false;
+    // console.log(is_drawing);
+    // context.strokeStyle = draw_color;
+    // context.lineWidth = draw_width;
+    // context.lineCap = "round";
+    // context.lineJoin = "round";
+
+    context.lineTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
+    // context.lineTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
+    context.stroke();
+  }
+  event.preventDefault();
+}
+function t_stop(event){
+    // console.log(pointX);
+  if (is_drawing){
+    context.closePath();
+    is_drawing= false;
+  }
+  // pointX=event.touches[0].pageX;
+  // console.log("this",pointX);
+
+  // if ( !mmoved ) {
+  //   mmoved=false;
+  //   console.log("this2",mmoved);
+  //   context.beginPath();
+  //   context.moveTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
+  //   context.lineWidth = 4 + parseInt(draw_width);
+  //
+  //   context.stroke();
+  //   context.closePath();
+  // }
+
+  // if ( event.type != "touch" ){
+  index += 1;
+  restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+  // }
+  event.preventDefault();
+}
+
+
 //mouse events...
 canvas.addEventListener("mousedown",start,false);
 canvas.addEventListener("mousemove",draw,false);
@@ -47,26 +114,30 @@ function check_start() {
 function start(event) {
   is_drawing = true;
   context.beginPath();
-  context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
-  context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+  // context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
+  context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+  // context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
   pointX = event.pageX;
   pointY = event.pageY;
   context.strokeStyle = draw_color;
   context.lineWidth = draw_width;
   context.lineCap = "round";
   context.lineJoin = "round";
+  context.shadowColor = draw_color;
+  context.shadowBlur = 2 ;
   // context.stroke();
   event.preventDefault();
 }
 function draw(event){
+  // console.log( event.touches[0].pageX );
   if(is_drawing){
-    context.strokeStyle = draw_color;
-    context.lineWidth = draw_width;
-    context.lineCap = "round";
-    context.lineJoin = "round";
-    context.shadowColor = draw_color;
-    context.shadowBlur = 2 ;
+    // context.strokeStyle = draw_color;
+    // context.lineWidth = draw_width;
+    // context.lineCap = "round";
+    // context.lineJoin = "round";
+
     context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    // context.lineTo(event.touches[0].pageX - canvas.offsetLeft, event.touches[0].pageY - canvas.offsetTop);
     context.stroke();
   }
   event.preventDefault();
@@ -78,7 +149,7 @@ function stop(event){
   }
   if ( pointX == event.pageX && pointY == event.pageY) {
     context.beginPath();
-    context.moveTo(event.pageX - canvas.offsetLeft,event.pageY - canvas.offsetTop);
+    context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
     context.lineWidth = 4+parseInt(draw_width);
     context.closePath();
     context.stroke();
